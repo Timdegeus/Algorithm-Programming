1c5de4a02942f744fb13c69d2ef2b34eabb5afba
BramVeninga <bram.veninga@student.nhlstenden.com>
2025-04-01 22:55:48 +0200

    added data structures

---
+++ Schietbaan/src/DataStructures/MyArray.java
@@ -0,0 +1,110 @@
+package DataStructures;
+
+public class MyArray<T>
+{
+    private T[] array;
+    private int size = 0;
+    private static final int INITIAL_CAPACITY = 10;
+
+    // Constructor
+    public MyArray()
+    {
+        array = (T[]) new Object[INITIAL_CAPACITY]; // Generics workaround
+    }
+
+    // Element toevoegen aan het einde
+    public void add(T element)
+    {
+        ensureCapacity();
+        array[size++] = element;
+    }
+
+    // Element toevoegen op een specifieke index
+    public void add(int index, T element)
+    {
+        if (index < 0 || index > size)
+        {
+            throw new IndexOutOfBoundsException("Index buiten bereik");
+        }
+        ensureCapacity();
+        for (int i = size; i > index; i--)
+        {
+            array[i] = array[i - 1];
+        }
+        array[index] = element;
+        size++;
+    }
+
+    // Element verwijderen op een specifieke index
+    public T remove(int index) {
+        if (index < 0 || index >= size)
+        {
+            throw new IndexOutOfBoundsException("Index buiten bereik");
+        }
+        T removedElement = array[index];
+        for (int i = index; i < size - 1; i++)
+        {
+            array[i] = array[i + 1];
+        }
+        array[--size] = null; // Vermijd geheugenlekken
+        return removedElement;
+    }
+
+    // Element ophalen
+    public T get(int index)
+    {
+        if (index < 0 || index >= size)
+        {
+            throw new IndexOutOfBoundsException("Index buiten bereik");
+        }
+        return array[index];
+    }
+
+    // Element instellen op een specifieke index
+    public void set(int index, T element)
+    {
+        if (index < 0 || index >= size)
+        {
+            throw new IndexOutOfBoundsException("Index buiten bereik");
+        }
+        array[index] = element;
+    }
+
+    // Controleert of de lijst leeg is
+    public boolean isEmpty()
+    {
+        return size == 0;
+    }
+
+    // Geeft de huidige grootte van de lijst terug
+    public int size()
+    {
+        return size;
+    }
+
+    // Alle elementen verwijderen
+    public void clear()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = null;
+        }
+        size = 0;
+    }
+
+    // Vergroot de capaciteit als de array vol is
+    private void ensureCapacity()
+    {
+        if (size == array.length)
+        {
+            int newCapacity = array.length * 2;
+            T[] newArray = (T[]) new Object[newCapacity];
+            for (int i = 0; i < size; i++)
+            {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+        }
+    }
+}
+
---
+++ Schietbaan/src/DataStructures/SinglyLinkedList.java
@@ -0,0 +1,144 @@
+package DataStructures;
+
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+public class SinglyLinkedList<T>
+{
+    private Node<T> head;
+    private int size = 0;
+
+    // Interne klasse Node
+    private static class Node<T>
+    {
+        T data;
+        Node<T> next;
+
+        Node(T data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Toevoegen aan het einde van de lijst
+    public void add(T element)
+    {
+        Node<T> newNode = new Node<>(element);
+        if (head == null)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node<T> current = head;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    // Toevoegen op een specifieke index
+    public void add(int index, T element)
+    {
+        if (index < 0 || index > size)
+        {
+            throw new IndexOutOfBoundsException("Index buiten bereik");
+        }
+        Node<T> newNode = new Node<>(element);
+        if (index == 0)
+        {
+            newNode.next = head;
+            head = newNode;
+        } else
+        {
+            Node<T> current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    // Verwijderen van een element op een bepaalde index
+    public T remove(int index) {
+        if (index < 0 || index >= size)
+        {
+            throw new IndexOutOfBoundsException("Index buiten bereik");
+        }
+        Node<T> removedNode;
+        if (index == 0)
+        {
+            removedNode = head;
+            head = head.next;
+        } else
+        {
+            Node<T> current = head;
+            for (int i = 0; i < index - 1; i++)
+            {
+                current = current.next;
+            }
+            removedNode = current.next;
+            current.next = current.next.next;
+        }
+        size--;
+        return removedNode.data;
+    }
+
+    // Verwijderen van een element (eerste voorkomen)
+    public boolean remove(T element)
+    {
+        if (head == null) return false;
+
+        if (head.data.equals(element))
+        {
+            head = head.next;
+            size--;
+            return true;
+        }
+
+        Node<T> current = head;
+        while (current.next != null && !current.next.data.equals(element))
+        {
+            current = current.next;
+        }
+
+        if (current.next == null) return false;
+
+        current.next = current.next.next;
+        size--;
+        return true;
+    }
+
+    // Ophalen van een element op een bepaalde index
+    public T get(int index) {
+        if (index < 0 || index >= size)
+        {
+            throw new IndexOutOfBoundsException("Index buiten bereik");
+        }
+        Node<T> current = head;
+        for (int i = 0; i < index; i++)
+        {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    // Geeft de grootte van de lijst terug
+    public int size()
+    {
+        return size;
+    }
+
+    // Controleert of de lijst leeg is
+    public boolean isEmpty()
+    {
+        return size == 0;
+    }
+
+}
